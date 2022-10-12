e9d090c7e212f4f6be0af7382f4358e783b2b4d1
ryanlozano07 <95672867+ryanlozano07@users.noreply.github.com>
2022-10-13 00:22:41 +0800

    delete

--- Website/TOOTHFULLY YOURS/TOOTHFULLY YOURS/login-register/assets/js/data.js
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from mongodb;
-const mongoose = require('mongoose');
-const con = await mongoose.connect(process.env.MONGO_URI);
-        
-
-
-MongoClient.connect(con, ()=>{
-    console.log("connected")
-} )
